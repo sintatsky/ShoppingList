da089268ef6a791245ef864c5e702388d276cc5d
Ilia <iliakreslov@gmail.com>
2021-10-19 13:30:29 +0300

    добавил ViewModel

--- app/src/main/java/com/example/shoppinglist/domain/GetShopItemByIdUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemByIdUseCase.kt
@@ -2,7 +2,7 @@ package com.example.shoppinglist.domain
 
 class GetShopItemByIdUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun getShopItemById(shopItemId: Int): ShopItem {
+    fun getShopItemById(shopItemId: Int): ShopItem? {
        return shopListRepository.getShopItemById(shopItemId)
     }
 }
