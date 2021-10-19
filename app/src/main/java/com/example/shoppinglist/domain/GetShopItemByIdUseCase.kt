60ec334bfc5de270c399f3039b588d499d824d10
Ilia <iliakreslov@gmail.com>
2021-10-19 11:51:20 +0300

    добавил в бизнес логику юзкейсы

---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemByIdUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.domain
+
+class GetShopItemByIdUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun getShopItemById(shopItemId: Int): ShopItem {
+       return shopListRepository.getShopItemById(shopItemId)
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,14 @@
+package com.example.shoppinglist.domain
+
+interface ShopListRepository {
+
+    fun addShopItem(shopItem: ShopItem)
+
+    fun editShopItem(shopItem: ShopItem)
+
+    fun getShopItemById(shopItemId: Int): ShopItem
+
+    fun getShopList(): List<ShopItem>
+
+    fun removeShopItem(shopItem: ShopItem)
+}
